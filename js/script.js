7ad0f46cceeb21f81c0cb9139cac89234edb5b24
LeonardoVFF <leonardovff@gmail.com>
2015-10-09 21:28:34 -0300

    Primeira versão

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var selectTree = (function(){
+	'use strict'
+	var teste = "emCod"
+	// selectors tree dom 
+	// get.item("seletor",fatherDom) 
+	// get.all("seletor",fatherDom) 
+	var get = {
+	 	item: function(el, root){
+			if(typeof(root)!="undefined") return root.querySelector(el);
+			return document.querySelector(el);
+	 	},
+	 	all: function(el, root){
+	 		if(typeof(root)!="undefined") return root.querySelectorAll(el);
+	 		return document.querySelectorAll(el);
+	 	}	
+	},
+	app = {
+		buildList: function(son){
+			var elOptions = get.all("option",son),
+			list = [],
+			arr = {};
+			for (var i = 0, lim = elOptions.length; i<lim; i++) {
+				//arr.father 
+				delete elOptions[i].dataset[teste];
+				console.log(elOptions[i].outerHTML);
+				// return 
+			};
+		}	
+	}
+	app.buildList(get.item(".teste"));
+
+	return function(eu){
+		console.log(eu);
+	}
+}());
+selectTree("eu"); 
