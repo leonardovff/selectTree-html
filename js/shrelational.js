4527a475dcf92b5fa1431d5ebfdbf9253fcee9bd
leonardovff <leonardovff@gmail.com>
2015-10-15 16:01:10 -0300

    Correções feitas, e solução para caso no select tenha mais de uma option com o mesmo value, simplesmente verifica se no array list já existe alguma option com tal valor, se tiver captura o indice onde ela se encontra e somente adiciona referencias aos pais presentes nesse outro option.

--- js/shrelational.js
+++ js/shrelational.js
@@ -10,21 +10,37 @@
 	 	all: function(el, root){
 	 		if(typeof(root)!="undefined") return root.querySelectorAll(el);
 	 		return document.querySelectorAll(el);
-	 	}	
+	 	},
+		searchKey: function(arr, key, value){
+			for (var i = arr.length; i--;) {
+				if(arr[i][key]==value) return i;
+			};
+			return null;
+		}
 	},
 	selectTree = (function(el){
 		var app = {
 			active: false,
 			list: [],
 			fathers: [],
+			el: {},
 			list: function(select){
 				var elOptions = get.all("option",select),
 				list = [],
 				dataset = {},
+				fathers = [],
+				valueCheck = [],
 				temp,
-				fathers = [];
+				val = 0,
+				keySearch;
 				for (var i = 0, lim = elOptions.length; i<lim; i++) {
-					temp = {};
+					val = elOptions[i].value
+					temp = {'value': val};
+					keySearch = null;
+					if(valueCheck.indexOf(val)!==-1) {
+						keySearch = get.searchKey(list, 'value', val);
+					}
+					if(keySearch!==null) temp = list[keySearch]
 					//PERCORRER OBJETO DATASET
 					dataset = elOptions[i].dataset;
 					//var keys = Object.keys(elOptions[i].dataset),key;
@@ -33,15 +49,23 @@
 					for (var key in dataset) if(dataset.hasOwnProperty(key)) {
 						// IF DATA OF SELECTTREE - @data-cod | @dataCod
 						if(key.indexOf("Cod")!=-1){
-							temp[key] = dataset[key].split(",");
+							if(keySearch!==null && key in temp) {
+								temp[key] = temp[key].concat(dataset[key].split(","));
+							} else {
+								temp[key] = dataset[key].split(",");
+							}
 							delete dataset[key];
 							if(!app.active) app.active = true;
 							if(fathers.indexOf(key)===-1) fathers.push(key);
 						}
 					}
-					temp['value'] = elOptions[i].value; 
-					temp.html = elOptions[i].outerHTML;
-					list.push(temp);
+					valueCheck.push(elOptions[i].value);
+					if(keySearch===null){
+						temp.html = elOptions[i].outerHTML;
+						list.push(temp);
+					} else {
+						list[keySearch] = temp;
+					}
 				};
 				app.fathers = fathers;
 				return list;
@@ -49,8 +73,9 @@
 		}
 		//FOR CLEAN FUNCTION LIST WITH LIST - CLEAN MEMORY
 		app.list = app.list(el);
-		if(!app.active) return 0;
-		console.log(app);
+		if(!app.active) return 0;	
+		app.el = el;
+		console.dir(app);
 	});
 	var el = get.all(selectTreeSelector);
 	for (var i = el.length - 1; i >= 0; i--) {
