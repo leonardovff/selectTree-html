7e64cb0ae8ac394d356e1473948840e175ef5325
leonardovff <leonardovff@gmail.com>
2015-10-16 15:17:42 -0300

    Feito a função de build das options dos selects com base nos valores selecionados nos selects pais, usando a função buildOptions e _filter para filtrar as options.

--- js/shrelational.js
+++ js/shrelational.js
@@ -24,16 +24,48 @@
 			list: [],
 			fathers: [],
 			el: {},
-			setValueFather: function(key){
+			_setValueFather: function(key){
 				var temp = get.item('select[data-shr-id="'+key+'"]'),
-				val = 0;
+				val;
 				if(temp===null) {
 					throw "SelectTree: Father ("+key+") not found";
 				}
 				val = temp.value;
-				if(typeof(temp.dataset['shr-value'])!=="undefined")
-					val = temp.dataset['shr-value']
+				if(typeof(temp.dataset['shrValue'])!=="undefined"){
+					val = temp.dataset['shrValue'];
+				}
 				app.fathers.push({'id': key,'value': val,'el': temp});
+			},
+			_updateValueFather: function(){
+				for (var i = app.fathers.length; i--;) {
+					app._setValueFather(app.fathers[i].id);
+				}
+			},
+			_filter: function(option){
+				for (var i = app.fathers.length; i--;) {
+					if(app.fathers[i].value!="all"){
+						if(typeof(option[app.fathers[i].id])==="undefined") return false;
+						// CONFERIR LANCE DE COMPRAÇÂO DE STRING COM NUMBER DO INDEX OF
+						if(option[app.fathers[i].id].indexOf(app.fathers[i].value)===-1) return false; 
+						console.log("entrou");
+					}
+				}
+				return true;
+			},
+			buildOptions: function(){
+				var string = "";
+				for (var i = app.list.length; i--;) {
+					if(app._filter(app.list[i])) 
+						string = app.list[i].html + string;
+				};
+				console.log(string);
+				app.el.innerHTML = string;
+			},
+			statusSelect: function(){
+
+			},
+			setEvents: function(){
+
 			},
 			list: function(select){
 				var elOptions = get.all("option",select),
@@ -54,11 +86,11 @@
 					if(keySearch!==null) listTemp = list[keySearch]
 					//PERCORRER OBJETO DATASET
 					dataset = elOptions[i].dataset;
+					for (var key in dataset) if(dataset.hasOwnProperty(key)) {
 					// var keys = Object.keys(elOptions[i].dataset),key;
 					// for (var i = keys.length - 1; i >= 0; i--) {
 					// 	key = keys[i];
-					for (var key in dataset) if(dataset.hasOwnProperty(key)) {
-						// IF DATA OF SELECTTREE - @data-cod | @dataCod
+						// IF HAVE DATA OF SELECTTREE - @data-cod | @dataCod
 						if(key.indexOf("Cod")!=-1){
 							if(keySearch!==null && key in listTemp) {
 								listTemp[key] = listTemp[key].concat(dataset[key].split(","));
@@ -68,7 +100,7 @@
 							delete dataset[key];
 							if(!app.active) app.active = true;
 							if(get.searchKey(app.fathers,'id',key)===null)
-								app.setValueFather(key);
+								app._setValueFather(key);
 						}
 					}
 					if(keySearch===null){
@@ -81,10 +113,11 @@
 				return list;
 			}
 		}
-		//FOR CLEAN FUNCTION LIST WITH ARRAY LIST - CLEAN MEMORY
-		app.list = app.list(el);
+		app.list = app.list(el); //FOR CLEAN FUNCTION LIST WITH ARRAY LIST - CLEAN MEMORY
 		if(!app.active) return 0;	
 		app.el = el;
+		app.buildOptions();
+		app.setEvents();
 		console.dir(app);
 	});
 	var el = get.all(selectTreeSelector);
